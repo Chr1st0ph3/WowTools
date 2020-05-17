4ca4e0617c6101da11c66a1774748843389318d9
Christophe Cremy <christophe.cremy@icloud.com>
2020-05-17 11:48:11 +0200

    Building

---
+++ Shell/wow_base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#-------------------------------------------------------------------------------
+# WowTools Shell - Base module
+#
+# Christophe Cremy, May
+#-------------------------------------------------------------------------------
+
+import sys
+
+def help():
+    print("Help!")
+
+
+def exit():
+    sys.exit()
